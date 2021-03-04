2d0334762487039cc1a0bbea96d58c5140deff83
NOELLA MICHIELS <NOELLA.MICHIELS@HVW-CAPAC.FGOV.BE>
2021-03-04 11:03:28 +0100

    Section 8

---
+++ RecipeShopping/src/app/shared/dropdown.directive.ts
@@ -0,0 +1,22 @@
+import { Directive, ElementRef, HostBinding, HostListener } from '@angular/core';
+
+@Directive({
+  selector: '[appDropdown]'
+})
+export class DropdownDirective {
+  @HostBinding('class.open') isOpen = false;
+
+  // Dropdown wordt gesloten bij het klikken op titel
+  // @HostListener('click') toggleOpen() {
+  //   this.isOpen = !this.isOpen;
+  // }
+
+  // a dropdown can also be closed by a click anywhere outside
+  // (which also means that a click on one dropdown closes any other one, btw.)
+  @HostListener('document:click', ['$event']) toggleOpen(event: Event) {
+    this.isOpen = this.elRef.nativeElement.contains(event.target) ? !this.isOpen : false;
+  }
+  constructor(private elRef: ElementRef) {}
+
+
+}
